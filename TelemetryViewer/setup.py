6fc5327f7b7f0fdf77d8f9f365aad4df9a7980b3
JoelESol <joelsol@telus.net>
2020-10-06 23:57:08 -0700

    Reupload and Retry

---
+++ TelemetryViewer/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TelemetryViewerSetup',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='joels',
+    author_email='',
+    description=''
+)
