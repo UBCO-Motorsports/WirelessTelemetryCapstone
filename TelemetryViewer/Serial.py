b85ad9a32177be6cadc1b97ae33b5dc98fd4fcad
Royden <roydencooke@gmail.com>
2020-11-03 10:25:39 -0800

    Working on Serial Port parsing Nov 1st

--- TelemetryViewer/Serial.py
+++ TelemetryViewer/Serial.py
@@ -1,19 +1,24 @@
-import serial
-import time
-import csv
+import serial,time,csv
+
+
 
 candata = serial.Serial(port='COM4', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1, xonxoff=False,
                         rtscts=False, dsrdtr=False)
-
 serial.Serial.reset_output_buffer(candata)   #clears input
+# except :
+#     print('Com port is not connected')
 
-with open('candatatry1.csv', 'w', newline='', encoding='utf8') as csvfile:
-    fieldnames = ['RPM', 'Lateral Acc', 'ThrottlePos']          #defining Columns of csv
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()                                        #Writing the headers to csv
-    for i in candata:                                           #Reads when data is incoming, timesout when defined
-        data = candata.readline()
-        data = data.decode('utf-8')
-        print(data)
-        csvfile.writelines(data)                                #Prints data to csv
-    csvfile.close()                                             #Close csv file
+if candata.isopen():
+
+
+
+    with open('candatatry1.csv', 'w', newline='', encoding='utf8') as csvfile:
+        fieldnames = ['RPM', 'Lateral Acc', 'ThrottlePos']          #defining Columns of csv
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()                                        #Writing the headers to csv
+        for i in candata:                                           #Reads when data is incoming, timesout when defined
+            data = candata.readline()
+            data = data.decode('utf-8')
+            print(data)
+            csvfile.writelines(data)                                #Prints data to csv
+        csvfile.close()                                             #Close csv file
