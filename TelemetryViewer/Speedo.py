5f0333a80fb4861f5761afa8f2952861065963a7
JoelESol <joelsol@telus.net>
2020-11-24 16:06:15 -0800

    Speedometer added
    Semifinished Speedo added. The needle is bouncy for some reason so I'll fix that later

---
+++ TelemetryViewer/Speedo.py
@@ -0,0 +1,79 @@
+import sys
+import platform
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect,
+                            QSize, QTime, QUrl, Qt, QEvent)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence,
+                           QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
+
+# GUI File
+
+from Speedometerwidgetroot import SpeedoQT
+
+Speed = 0
+
+
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = SpeedoQT()
+        self.ui.setupUi(self)
+        self.animate(0)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.accellerate)
+        self.timer.start(15)
+
+        self.show()
+    def accellerate (self):
+        global Speed
+        value=Speed
+
+        self.animate(value)
+        if Speed > 150:
+            self.timer.stop()
+
+            self.close()
+        Speed += 0.5
+
+
+    def animate(self,value):
+        global Speed
+        value=Speed
+        htmlText=" "
+        newHtml=htmlText.replace("{Value}",str(value))
+        self.ui.label_3.setText(newHtml)
+
+        t = QtGui.QTransform()
+        # -222 and 45
+        if value>120:
+            value=120
+        if value<0:
+            value=0
+        value=reMap(Speed,120, 0,239,0)
+        t.rotate(value)
+        # load your image
+        image = QtGui.QImage("needle3.png")
+        pixmap = QtGui.QPixmap.fromImage(image)
+        # rotate the pixmap
+        rotated_pixmap = pixmap.transformed(t)
+        self.ui.Needle.setPixmap(rotated_pixmap)
+
+
+def reMap(value, maxInput, minInput, maxOutput, minOutput):
+    value = maxInput if value > maxInput else value
+    value = minInput if value < minInput else value
+
+    inputSpan = maxInput - minInput
+    outputSpan = maxOutput - minOutput
+
+    scaledThrust = float(value - minInput) / float(inputSpan)
+
+    return minOutput + (scaledThrust * outputSpan)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SplashScreen()
+    sys.exit(app.exec_())
