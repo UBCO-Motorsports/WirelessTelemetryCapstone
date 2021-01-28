fa575d367c0360c07c0784050e40aaa5887a1be7
JoelESol <joelsol@telus.net>
2021-01-28 12:04:38 -0800

    Thing update

--- TelemetryViewer/MainWindow.py
+++ TelemetryViewer/MainWindow.py
@@ -112,28 +112,29 @@ class MainWindow(QtWidgets.QMainWindow):
         print(self.availableCOMPorts())
 
     def applytoConfig(self):
-        with open('itemslogged.json', 'r+') as json_file:
-            data = json.load(json_file)
-            json_file.close()
-        layout = QVBoxLayout()
-        self.radiodict={}
-        for i in range(len(data["logged"])):
-            name = data["logged"][i]['Name']
-            id = data["logged"][i]['ID']
-            units = data["logged"][i]['Units']
-            scale = data["logged"][i]['Scale']
-            position = data["logged"][i]['Position']
-            size = data["logged"][i]['Size']
-            offset = data["logged"][i]['Offset']
-            color = data["logged"][i]['Color']
-            self.radiodict[i]=QCheckBox(name)
-            self.ui.comboBox_3.addItem(name)
-            self.ui.comboBox_4.addItem(name)
-            self.ui.comboBox_5.addItem(name)
-            self.ui.comboBox.addItem(name)
-        for i in self.radiodict.items():
-            layout.addWidget(i[1])
-        self.ui.scrollArea.setLayout(layout)
+        if self.ui.listWidget.item(0).text() != "None Selected":
+            with open('itemslogged.json', 'r+') as json_file:
+                data = json.load(json_file)
+                json_file.close()
+            layout = QVBoxLayout()
+            self.radiodict={}
+            for i in range(len(data["logged"])):
+                name = data["logged"][i]['Name']
+                id = data["logged"][i]['ID']
+                units = data["logged"][i]['Units']
+                scale = data["logged"][i]['Scale']
+                position = data["logged"][i]['Position']
+                size = data["logged"][i]['Size']
+                offset = data["logged"][i]['Offset']
+                color = data["logged"][i]['Color']
+                self.radiodict[i]=QCheckBox(name)
+                self.ui.comboBox_3.addItem(name)
+                self.ui.comboBox_4.addItem(name)
+                self.ui.comboBox_5.addItem(name)
+                self.ui.comboBox.addItem(name)
+            for i in self.radiodict.items():
+                layout.addWidget(i[1])
+            self.ui.scrollArea.setLayout(layout)
 
 
 
@@ -224,9 +225,9 @@ class MainWindow(QtWidgets.QMainWindow):
         global Canfilename
         found = False
         for i in range(self.ui.listWidget.count()):
-            if self.ui.listWidget.item(i).text() == self.ui.tableWidget.item(row, 1).text():
+            if self.ui.listWidget.item(i).text() == self.ui.tableWidget.item(row, 1).text() and self.ui.listWidget.count()<16:
                 found = True
-        if not found:
+        if not found and self.ui.listWidget.count()<16:
             color = QColorDialog.getColor()
             color2 = color.getRgb()
             if color.isValid():
